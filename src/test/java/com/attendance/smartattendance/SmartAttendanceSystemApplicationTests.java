ac02ba7d2cb241125f16534184b27238a5249f7c
Babul Kumar <drbyaduvanshi@gmail.com>
2026-08-31 15:41:03 +0530

    Refactor application class to test context loading

--- src/test/java/com/attendance/smartattendance/SmartAttendanceSystemApplicationTests.java
+++ src/test/java/com/attendance/smartattendance/SmartAttendanceSystemApplicationTests.java
@@ -1,15 +1,13 @@
 package com.attendance.smartattendance;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class SmartAttendanceSystemApplication {
+@SpringBootTest
+class SmartAttendanceSystemApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(
-            SmartAttendanceSystemApplication.class,
-            args
-        );
+    @Test
+    void contextLoads() {
     }
+
 }
