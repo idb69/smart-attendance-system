e4ea20da1f618b96bef0c69a5cca66e4735a0170
Babul Kumar <drbyaduvanshi@gmail.com>
2026-08-31 16:09:49 +0530

    Refactor application test class for online exam system

--- src/test/java/com/onlineexam/system/OnlineExamSystemApplicationTests.java
+++ src/test/java/com/onlineexam/system/OnlineExamSystemApplicationTests.java
@@ -1,12 +1,12 @@
-package com.smartattendance;
+package com.onlineexam.system;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class SmartAttendanceSystemApplication {
+@SpringBootTest
+class OnlineExamSystemApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SmartAttendanceSystemApplication.class, args);
+    @Test
+    void contextLoads() {
     }
 }
