4ab9398a3c404a4dba4a0619dc7630292b49d17f
Babul Kumar <drbyaduvanshi@gmail.com>
2026-08-31 15:39:05 +0530

    Refactor test class into main application class

--- src/test/java/com/attendance/smartattendance/SmartAttendanceSystemApplicationTests.java
+++ src/test/java/com/attendance/smartattendance/SmartAttendanceSystemApplicationTests.java
@@ -1,13 +1,15 @@
 package com.attendance.smartattendance;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class SmartAttendanceSystemApplicationTests {
+@SpringBootApplication
+public class SmartAttendanceSystemApplication {
 
-	@Test
-	void contextLoads() {
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(
+            SmartAttendanceSystemApplication.class,
+            args
+        );
+    }
 }
